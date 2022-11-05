c14b1b5d9e5ca5d2a8371b0008e0d0a80391b3b5
syihabudin081 <syihabupnyk@gmail.com>
2022-11-05 16:14:08 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,106 @@
+const { nanoid } = require('nanoid');
+
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const finished = readPage === pageCount;
+  const insertedAt = new Date().toISOString;
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    finished,
+    updatedAt,
+  };
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'error',
+    message: 'Buku gagal ditambahkan',
+  });
+
+  response.code(500);
+  return response;
+};
+
+// const getBookbyIdHandler = (request, h) => {
+
+// };
+const getBookHandler = (request, h) => {
+//   const { id, name, publisher } = request.query;
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books,
+    },
+  });
+  response.code(200);
+  return response;
+};
+
+// const editBookbyIdHandler = (request, h) => {
+
+// };
+// const deleteBookbyIdHandler = (request, h) => {
+
+// };
+
+module.exports = {
+  //    getBookbyIdHandler, getBookHandler, editBookbyIdHandler, deleteBookbyIdHandler,
+  addBookHandler, getBookHandler,
+};
---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  addBookHandler, getBookHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getBookHandler,
+  },
+//   {
+//     method: 'GET',
+//     path: '/books/{bookId}',
+//     handler: getBookbyIdHandler,
+//   },
+//   {
+//     method: 'PUT',
+//     path: '/books/{bookId}',
+//     handler: editBookbyIdHandler,
+//   },
+//   {
+//     method: 'DELETE',
+//     path: '/books/{bookId}',
+//     handler: deleteBookbyIdHandler,
+//   },
+];
+
+module.exports = routes;
